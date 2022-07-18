7cd53322e8416456471087f3974423da82bd5a0c
Grace Cham <hscham@chromium.org>
2022-07-18 22:24:48 +0900

    libchrome: add libchrome namespace to perfetto by script
    Instead of hardcoded patch file which is prone to patching error when
    there is a new code base, hence increasing manual workload of creating a
    new libchrome uprev commit.
    
    BUG=b:236092354
    TEST=emerge-hatch libchrome perfetto_simple_producer
    
    Cq-Depend: chromium:3758443
    Change-Id: I782e318c3a9aaf1f384648658be4d64eeccaaaca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/libchrome/+/3770047
    Reviewed-by: Qijiang Yūki Ishii <fqj@google.com>
    Commit-Queue: Grace Cham <hscham@chromium.org>
    Tested-by: Grace Cham <hscham@chromium.org>

---
+++ libchrome_tools/patches/add-libchrome-namespace-to-trace_event-stub-perfetto.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Add libchrome namespace to libchrome's perfetto stubs.
+#
+# chromeos-base/perfetto_simple_producer and possibly other libchrome clients
+# include the real perfetto as their dependency. On the other hand, they also uses
+# parts of libchrome which will be indirectly including the trace_event_stub.h and
+# causes redefinition error.
+#
+# Change to perfetto_libchrome namspace for the stub ones used within libchrome
+# only.
+
+for f in $(grep -rl perfetto:: --exclude-dir=libchrome_tools/); do
+  sed -i 's/perfetto::/perfetto_libchrome::/g' ${f}
+done
+
+for f in $(grep -rl "namespace perfetto {" --exclude-dir=libchrome_tools/); do
+  sed -i 's/namespace perfetto {/namespace perfetto_libchrome{/g' ${f}
+done
