e6dcc8f9837f1c8f09e6a8cdeb08473424a30b7a
Ryo Hashimoto <hashimoto@chromium.org>
2023-08-01 22:07:45 +0900

    Add a script to apply libchrome pathces
    This script replaces the shell script in libchrome ebuild,
    and it'll be used by both ebuild and libchrome developers who need to
    try applying libchrome patches.
    
    BUG=b:293001194
    TEST=./libchrome_tools/apply_patches.py
    
    Change-Id: I1a5c29c79e8d7004757383d90f6c62308784efda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/libchrome/+/4740233
    Tested-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Grace Cham <hscham@chromium.org>

---
+++ libchrome_tools/apply_patches.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# Copyright 2023 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""
+Tool to apply libchrome patches.
+"""
+
+import argparse
+import glob
+import logging
+import os
+import stat
+import subprocess
+import sys
+
+
+def main() -> None:
+    logging.basicConfig(level=logging.INFO)
+
+    parser = argparse.ArgumentParser(description="Apply libchrome patches.")
+    parser.add_argument(
+        "--libchrome-checkout-path",
+        default=os.getcwd(),
+        required=False,
+        help="Path of the libchrome checkout. Defaults to the current directory.",
+    )
+    args = parser.parse_args()
+
+    # Apply all patches in directory, ordered by type then patch number.
+    PREFIXES = [
+        "long-term",
+        "cherry-pick",
+        "backward-compatibility",
+        "forward-compatibility",
+    ]
+    for prefix in PREFIXES:
+        for patch in sorted(
+            glob.glob(
+                "%s/libchrome_tools/patches/%s-*"
+                % (args.libchrome_checkout_path, prefix)
+            )
+        ):
+            logging.info("Applying %s...", patch)
+            if patch.endswith(".patch"):
+                cmd_args = [
+                    "patch",
+                    "--strip=1",
+                    "--force",
+                    "--get=0",
+                    "--no-backup-if-mismatch",
+                    "--quiet",
+                    "--input",
+                    patch,
+                ]
+            elif os.stat(patch).st_mode & stat.S_IXUSR != 0:
+                cmd_args = [patch]
+            else:
+                raise RuntimeError("Invalid patch file %s" % patch)
+
+            subprocess.check_call(cmd_args)
+
+
+if __name__ == "__main__":
+    main()
