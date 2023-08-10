cdade1ca40231bf3ebf9115ce516edf810093c5f
Grace Cham <hscham@chromium.org>
2023-08-10 11:16:16 +0900

    libchrome: apply_patches: add --dry-run mode for debugging
    Which would log commands that apply patches as debug statements instead
    of running them. This will make debugging easier for developing more
    features later since it saves the time to clean up the git repository.
    
    BUG=b:293001194
    TEST=sudo emerge libchrome
    TEST=run command in libchrome repo with and without --dry-run
    
    Change-Id: Ifde59027b703d6f2c35b705dcca5761aa43e0f30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/libchrome/+/4768537
    Tested-by: Grace Cham <hscham@chromium.org>
    Auto-Submit: Grace Cham <hscham@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Grace Cham <hscham@chromium.org>

--- libchrome_tools/apply_patches.py
+++ libchrome_tools/apply_patches.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 import stat
 import subprocess
+from typing import Sequence
 
 PREFIXES = [
     "long-term",
@@ -22,30 +23,40 @@ PREFIXES = [
     "forward-compatibility",
 ]
 
+def _run_or_log_cmd(cmd: Sequence[str], dry_run: bool) -> None:
+    if dry_run:
+        logging.info("$ %s", " ".join(cmd))
+    else:
+        subprocess.check_call(cmd)
 
-def apply_patch(patch: str, ebuild: bool):
+
+def _git_apply_patch(patch: str, threeway: bool, dry_run: bool) -> None:
+    # "-C1" to be compatible with `patch` and to be more robust against upstream
+    # changes.
+    git_apply_cmd = [ "git", "apply", "-C1", patch ]
+    if threeway:
+        git_apply_cmd.append("--3way")
+    _run_or_log_cmd(git_apply_cmd, dry_run)
+
+
+def apply_patch(patch: str, ebuild: bool, dry_run: bool) -> None:
     """Applying given patch."""
     if patch.endswith(".patch"):
-        # "-C1" to be compatible with `patch` and to be more robust against
-        # upstream changes.
         try:
-            subprocess.check_call(
-                [ "git", "apply", "-C1", patch ])
+            _git_apply_patch(patch, False, dry_run)
         except subprocess.CalledProcessError as err:
             if not ebuild:
                 # Rerun (expected failure) to leave a 3-way merge marker.
-                subprocess.check_call([ "git", "apply", "-C1", patch, "--3way"])
+                _git_apply_patch(patch, True, dry_run)
             else:
                 raise RuntimeError(f"Failed to apply patch {patch}.") from err
     elif os.stat(patch).st_mode & stat.S_IXUSR != 0:
-        subprocess.check_call([patch])
+        _run_or_log_cmd([patch], dry_run)
     else:
         raise RuntimeError(f"Invalid patch file {patch}.")
 
 
 def main() -> None:
-    logging.basicConfig(level=logging.INFO)
-
     parser = argparse.ArgumentParser(description="Apply libchrome patches.")
     parser.add_argument(
         "--libchrome-path",
@@ -61,7 +72,16 @@ def main() -> None:
         action="store_true",
         help=("Run from ebuild where the libchrome directory is not a git "
               "repository. Defaults to False."))
+    parser.add_argument(
+        "--dry-run",
+        default=False,
+        required=False,
+        action="store_true",
+        help=("dry run mode where patches are not really applied. Log as debug "
+              "statements instead. Defaults to False."))
     args = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO)
 
     # In non-ebuild mode, change to libchrome directory which should be a git
     # repository for git commands like am and commit at the right repository.
@@ -77,7 +97,7 @@ def main() -> None:
     for prefix in PREFIXES:
         for patch in sorted(glob.glob(f"libchrome_tools/patches/{prefix}-*")):
             logging.info("Applying %s...", patch)
-            apply_patch(patch, args.ebuild)
+            apply_patch(patch, args.ebuild, args.dry_run)
 
 
 if __name__ == "__main__":
